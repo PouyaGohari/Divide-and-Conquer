17d8bfb34cfddfcd9b1d4d8c0db7e2ef0cc35b51
PouyaGohari <pouya.hmgohari@gmail.com>
2023-10-21 19:41:59 +0330

    Add a dictionary and extra array for my algorithm

--- send_secret_message.cpp
+++ send_secret_message.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+vector<int> dictionary(string input){
+    vector<int> dict_array(26, 0);
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(auto chr : input){
+        dict_array[chr - 'a'] = 1; 
+    }
+    return dict_array;
+}
+
 int main(){
+    
     return 0;
 }
