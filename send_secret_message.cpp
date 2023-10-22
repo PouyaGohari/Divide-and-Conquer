191840615fd199a87d09fe1c8249a589911095e6
PouyaGohari <pouya.hmgohari@gmail.com>
2023-10-22 13:36:45 +0330

    Add a helper array to the question

--- send_secret_message.cpp
+++ send_secret_message.cpp
@@ -20,8 +20,19 @@ vector<int> dictionary(string input){
     return dict_array;
 }
 
+vector<int> help_array(string input, vector<int> dictionary){
+    vector<int> helping_array(input.size(), 0);
+    for(int i = 0; i < input.size(); i++){
+        if(dictionary[input[i]-'a'] == 1 && dictionary[26-(input[i]-'a')-1] == 1){
+            helping_array[i] = 1;
+        }
+    }
+    return helping_array;
+}
+
 int main(){
     string input = getting_input();
     vector<int> dict_array = dictionary(input);
+    vector<int> helping_array = help_array(input, dict_array);
     return 0;
 }
